921ca983ac80d9d4a9403509180047ef26b4cffe
João Victor Almeida <61315569+joaovictorgit@users.noreply.github.com>
2023-02-07 10:34:52 -0300

    Update SocialNetworks.js

--- src/components/SocialNetworks.js
+++ src/components/SocialNetworks.js
@@ -2,9 +2,9 @@ import '../styles/components/socialnetworks.sass';
 import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa';
 
 const socialNetworks = [
-  { name: 'linkedin', icon: <FaLinkedinIn/>, link: 'https://www.instagram.com/joaovictorfelix2001/?hl=pt-br'},
+  { name: 'linkedin', icon: <FaLinkedinIn/>, link: 'www.linkedin.com/in/joaovictordev'},
   { name: 'github', icon: <FaGithub />, link: 'https://github.com/joaovictorgit'},
-  { name: 'instagram', icon: <FaInstagram />, link: 'https://www.linkedin.com/in/jo%C3%A3o-v-a41a9313b/'},
+  { name: 'instagram', icon: <FaInstagram />, link: 'https://www.instagram.com/joaovictorfelix2001/?hl=pt-br'},
 ];
 
 function SocialNetworks() {
