98b1b38295202f98dac7dd96be487f75dfe89f83
joaovictorgit <victoralmeida.0216@gmail.com>
2023-02-13 15:58:07 -0300

    Send change files

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <section className='about-container'>
-        <h2>Sobre</h2>
-        <p>
-            Olá, sou João Victor. Atualmente estou cursando Ciência da Computação, na Universidade 
-            Federal do Ceará, Campus de Quixadá. Tive contato com a programação em 2016 quando iniciei
-            meus estudos na Escola Estadual de Educação Profissional Venceslau Viera Batista, no curso 
-            de informática.
-        </p>
-        <p>
-            Desde então venho aprendendo novas tecnologias e aperfeiçoando meus conhecimentos nas quais 
-            eu já tenho conhecimento. Atualmente estou desenvolvendo mais aplicações Web utilizando React JS 
-            e também aplicações em dispositivos móveis em React Native e em Java.
-        </p>
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Olá, meu nome é João Victor , e sou um desenvolvedor full stack júnior.
+        Tenho 2 anos de experiência em desenvolvimento de software e estou
+        ansioso para aplicar minhas habilidades e conhecimentos para contribuir
+        com a equipe. Minha paixão pelo desenvolvimento de software começou
+        quando eu tinha 20 anos e desde então tenho estudado e praticado
+        constantemente. Tenho experiência em desenvolvimento web, trabalhando
+        com tecnologias como HTML, CSS, JavaScript, React e Node.js. Além disso,
+        tenho conhecimento em banco de dados, como MySQL e MongoDB.
+      </p>
+      <p>
+        Estou animado para continuar aprendendo e crescendo como desenvolvedor
+        full stack. Acredito que posso ser um valioso membro da equipe, ajudando
+        a resolver problemas e construir soluções inovadoras.
+      </p>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/ListProjects.js
+++ src/components/ListProjects.js
@@ -1,28 +1,96 @@
-import {DiReact, DiJava} from 'react-icons/di';
-import '../styles/components/listprojects.sass';
+import { DiReact, DiJava, DiNodejsSmall } from "react-icons/di";
+import { SiTypescript } from "react-icons/si";
+import "../styles/components/listprojects.sass";
 
 const listProjects = [
-    {id: 'reactjs', name: 'Pokedex', icon: <DiReact />, link: 'https://github.com/joaovictorgit/pokedex', description: 'Aplicação desenvolvida em React JS e styled-components'},
-    {id: 'reactnative', name: 'Music Player', icon: <DiReact />, link: 'https://github.com/joaovictorgit/Player_Music', description: 'Aplicação desenvolvida em React Native com TypeScript'},
-    {id: 'java', name: 'Drink Water', icon: <DiJava />, link: 'https://github.com/joaovictorgit/beber_agua', description: 'Aplicação de dispositivo móvel desenvolda em Java'},
+  {
+    id: "reactjs",
+    name: "Pokedex",
+    icon: <DiReact />,
+    link: "https://github.com/joaovictorgit/pokedex",
+    description: "Aplicação desenvolvida em React JS e styled-components",
+  },
+  {
+    id: "reactnative",
+    name: "Music Player",
+    icon: <DiReact />,
+    link: "https://github.com/joaovictorgit/Player_Music",
+    description: "Aplicação desenvolvida em React Native com TypeScript",
+  },
+  {
+    id: "java",
+    name: "Drink Water",
+    icon: <DiJava />,
+    link: "https://github.com/joaovictorgit/beber_agua",
+    description: "Aplicação de dispositivo móvel desenvolda em Java",
+  },
+  {
+    id: "nodejs",
+    name: "Music Boot",
+    icon: <DiNodejsSmall />,
+    link: "https://github.com/joaovictorgit/music-bot",
+    description:
+      "Aplicação, desenvolvida usando node.js, que cria um bot no discord que toca música. ",
+  },
+  {
+    id: "typescript",
+    name: "Chat Server",
+    icon: <SiTypescript />,
+    link: "https://github.com/joaovictorgit/ChatServer",
+    description:
+      "Aplicação, desenvolvida em React que mensagens em tempo real.",
+  },
+  {
+    id: "nodejs",
+    name: "Ebook",
+    icon: <DiNodejsSmall />,
+    link: "https://github.com/joaovictorgit/Ebook",
+    description: "API, desenvolvida usando Typescript, de uma loja de livros.",
+  },
+  {
+    id: "typescript",
+    name: "Book Store",
+    icon: <SiTypescript />,
+    link: "https://github.com/joaovictorgit/BookStore",
+    description:
+      "Aplicação, desenvolvida em React Js, que utiliza os dados da API Ebook.",
+  },
+  {
+    id: "reactjs",
+    name: "Games Shop",
+    icon: <DiReact />,
+    link: "https://github.com/joaovictorgit/Ecommerce-games",
+    description:
+      "Aplicação, desenvolvida em React Js, de um E-commerce de Games",
+  },
+  {
+    id: "reactjs",
+    name: "List Task",
+    icon: <DiReact />,
+    link: "https://github.com/joaovictorgit/List-Task",
+    description:
+      "Aplicativo, desenvolvido com React Native, que realiza um CRUD usando Firebase.",
+  },
 ];
 
 function ListProjects() {
   return (
-    <section className='list-container'>
-        <div className='list-grid'>
-            {listProjects.map((list) => (
-            <div className='list-card' id={list.id} key={list.id}>
-                {list.icon}
-                <div className='list-info'>
-                    <h3><a href={list.link}>{list.name}</a></h3>
-                    <p>{list.description}</p>
-                </div>
+    <section className="list-container">
+      <div className="list-grid">
+        {listProjects.map((list, index) => (
+          <div className="list-card" id={list.id} key={index}>
+            {list.icon}
+            <div className="list-info">
+              <h3>
+                <a href={list.link}>{list.name}</a>
+              </h3>
+              <p>{list.description}</p>
             </div>
-            ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 
-export default ListProjects
+export default ListProjects;
